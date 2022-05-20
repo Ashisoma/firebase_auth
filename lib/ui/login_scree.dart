4fb0349a4378ec32ae94f480c2abe917d19b2b66
Ashisoma <okalionesmus@gmail.com>
2022-05-20 22:33:18 +0300

    forms

--- lib/ui/login_scree.dart
+++ lib/ui/login_scree.dart
@@ -1,17 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({ Key? key }) : super(key: key);
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //form key
+  final _formKey = GlobalKey<FormState>();
+
+  // edit controller
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (value) {
+        emailController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.mail),
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Email",
+        border:OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+
+    final passwordFIeld = TextFormField(
+      autofocus: false,
+      controller: passwordController,
+      onSaved: (value) {
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+        prefixIcon: Icon(Icons.key),
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Password",
+        border:OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child:SingleChildScrollView(
+          child: Container(
+             color: Colors.white,
+             child: Form(
+               key: _formKey,
+               child: Column(
+                 children: [
+                   emailField,
+                   passwordFIeld
+                 ],
+               ),
+               ),
+          ),
+        ),
+      ),
     );
   }
 }
