80263d244d659902739385ba8db24b53d1e2271f
Ashisoma <okalionesmus@gmail.com>
2022-05-25 10:18:19 +0300

    user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,4 +3,23 @@ class UserModel {
   String? email;
   String? firstName;
   String? secondName;
+
+  UserModel({this.uuid, this.email, this.firstName, this.secondName});
+
+  factory UserModel.fromMap(map) {
+    return new UserModel(
+        uuid: map['uuid'],
+        email: map['email'],
+        firstName: map['firstName'],
+        secondName: map['secondName']);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uuid':uuid,
+      'email':email,
+      'firstName':firstName,
+      'secondName':secondName,
+    };
+  }
 }
